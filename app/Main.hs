12b63ef8e7c4ed9b10256c491e5c0f235f097665
Gandalfthegreat <785918558@qq.com>
2021-12-05 14:35:19 -0800

    add brick main process

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,15 @@
 module Main where
 
+import Brick.AttrMap
+import Brick.Main
+import Brick.Types
+import qualified Brick.Widgets.Border as B
+import qualified Brick.Widgets.Center as C
+import Brick.Widgets.Core
+import Control.Monad (void)
+import Graphics.Vty.Input.Events
 import Lib
 
 main :: IO ()
-main = someFunc
+main = do
+  void $ defaultMain app initialState
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,7 +1,52 @@
-module Lib
-  ( someFunc,
-  )
-where
+module Lib where
 
-someFunc :: IO ()
-someFunc = putStr "HornChess"
+-- main library of Brick
+
+import Brick.AttrMap
+import Brick.Main
+import Brick.Types
+import Brick.Widgets.Border (borderWithLabel, vBorder)
+import qualified Brick.Widgets.Border as B
+import qualified Brick.Widgets.Center as C
+import Brick.Widgets.Core
+import Graphics.Vty.Input.Events
+
+data St = St
+  { board :: String
+  }
+
+initialState =
+  St
+    { board = "X X O"
+    }
+
+app :: App St e ()
+app =
+  App
+    { appDraw = draw, -- UI function
+      appChooseCursor = const . const Nothing, -- don't need cursor
+      appHandleEvent = handleEvent, -- handle event to change state
+      appStartEvent = return,
+      appAttrMap = const $ attrMap mempty [] -- add style to UI
+    }
+
+-- buildInitialState :: IO TuiState
+-- buildInitialState = pure
+draw :: St -> [Widget ()]
+draw st = [ui]
+  where
+    ui =
+      vBox
+        [ str "HornChess",
+          str "(Press Esc to quit)"
+        ]
+
+handleEvent :: St -> BrickEvent () e -> EventM () (Next St)
+handleEvent s e =
+  case e of
+    VtyEvent vtye ->
+      --- press q t exit the program
+      case vtye of
+        EvKey KEsc [] -> halt s
+        _ -> continue s
+    _ -> continue s
