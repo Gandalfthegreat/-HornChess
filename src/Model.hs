4c8691312d7a804f22377227960fc3651e5069a2
Gandalfthegreat <785918558@qq.com>
2021-12-07 22:01:46 -0800

    already drawn half board

--- src/Model.hs
+++ src/Model.hs
@@ -9,7 +9,14 @@ data Chess
   = Black1
   | Black2
   | White
-  | Empty
+  | Empty -- board in black
+  deriving (Eq, Show)
+
+data Pixel
+  = BlackChess -- pixel for black chess (virtually Yellow)
+  | WhiteChess -- pixel for white chess (virtually White)
+  | Background -- pixel for bg (virtually grey)
+  | Board -- pixel for board (virtually black)
   deriving (Eq, Show)
 
 initialState =
--- src/View.hs
+++ src/View.hs
@@ -40,14 +40,24 @@ import Control.Monad.IO.Class (liftIO)
 import qualified Data.Map as M
 import Data.Monoid
 import Data.Sequence (Seq, fromList)
+import Graphics.Vty (black)
 import qualified Graphics.Vty as V
 import Lens.Micro ((%~), (&), (.~), (^.))
 import Lens.Micro.TH (makeLenses)
 import Linear.V2 (V2 (..), _x, _y)
 import Model
 
+---------------------------------------------------------constant------------------------------------------------------------------
 type Name = ()
 
+cw :: Widget Name
+cw = str "*" -- pixel is 2 spaces
+
+height = 58
+
+width = 64
+
+--------------------------------------------------draw function-----------------------------------------------------------------------------------------------------
 draw :: St -> [Widget Name]
 draw st = [C.center (drawGrid st)]
 
@@ -64,31 +74,87 @@ drawGrid st =
 
     -- cellAt c
     --   | otherwise = Empty
-    cellAt c = Empty
-
---  isCellPartOfDiskSet c diskSet =
---      any (==True) $ fmap isPartOfDisk [0,1,2] -- for each disk check
---    where
---      isPartOfDisk  i = c `elem` ( diskSet M.! i)
---  isCellPartOfPegSet c =  c `elem` towerPegSet
+    cellAt c -- give a coordinate, return a state of pixel
+      | isPartOfBoard c = Board
+      | isPartOfWhite c = WhiteChess
+      | isPartOfBlack c = BlackChess
+      | otherwise = Background
 
 -- draw a cell
-drawCell :: Chess -> Widget Name
--- drawCell WHITE = withAttr boardAttr cw
--- drawCell WHITE    = withAttr blackAttr cw
--- drawCell Empty      = withAttr whiteAttr cw
-drawCell Empty = withAttr bgAttr cw
-drawCell _ = withAttr bgAttr cw
+drawCell :: Pixel -> Widget Name
+drawCell Board = withAttr boardAttr cw
+drawCell Background = withAttr bgAttr cw
+drawCell WhiteChess = withAttr whiteAttr cw
+drawCell BlackChess = withAttr blackAttr cw
 
--- pixel is 2 spaces
-cw :: Widget Name
-cw = str " "
+--------------------------------------------------get cell to render -------------------------------------
+isPartOfBoard c = c `elem` pegBase
 
-height = 58
+isPartOfBlack c = c `elem` blackList
 
-width = 64
+isPartOfWhite c = c `elem` whiteList
+
+whiteList = [V2 01 02]
+
+blackList = [V2 15 18, V2 19 30]
+
+--------------------------------------------------hard code chess-------------------------------------
+pegBase = horizontalLine ++ zone1 ++ zone2 ++ zone3 ++ zone4 ++ zone5 ++ zone6
+
+horizontalLine =
+  fmap (\x -> V2 x 01) [5 .. 42]
+    ++ fmap (\x -> V2 x 13) [9 .. 43]
+    ++ fmap (\x -> V2 x 25) [13 .. 45]
+    ++ fmap (\x -> V2 x 35) [18 .. 46]
+    ++ fmap (\x -> V2 x 43) [26 .. 49]
+    ++ fmap (\x -> V2 x 49) [35 .. 53]
+    ++ fmap (\x -> V2 x 56) [53 .. 58]
+
+-- zone is from bottom to up
+zone1 =
+  fmap (\x -> V2 x 2) ([5, 38 .. 40] ++ [42])
+    ++ fmap (\x -> V2 x 3) ([5, 35 .. 37] ++ [42])
+    ++ fmap (\x -> V2 x 4) ([6, 32 .. 34] ++ [42])
+    ++ fmap (\x -> V2 x 5) ([6, 29 .. 31] ++ [42])
+    ++ fmap (\x -> V2 x 6) ([7, 26 .. 28] ++ [42])
+    ++ fmap (\x -> V2 x 7) ([7, 23 .. 25] ++ [43])
+    ++ fmap (\x -> V2 x 8) ([7, 20 .. 22] ++ [43])
+    ++ fmap (\x -> V2 x 9) ([8, 17 .. 19] ++ [43])
+    ++ fmap (\x -> V2 x 10) ([8, 14 .. 16] ++ [43])
+    ++ fmap (\x -> V2 x 11) ([9, 11 .. 13] ++ [43])
+    ++ fmap (\x -> V2 x 12) ([9, 10] ++ [43])
+
+zone2 =
+  fmap (\x -> V2 x 14) ([9, 40, 41, 43])
+    ++ fmap (\x -> V2 x 15) ([9, 37 .. 39] ++ [43])
+    ++ fmap (\x -> V2 x 16) ([10, 35, 36, 43])
+    ++ fmap (\x -> V2 x 17) ([10, 32 .. 34] ++ [44])
+    ++ fmap (\x -> V2 x 18) ([10, 30, 31, 44])
+    ++ fmap (\x -> V2 x 19) ([11, 27 .. 29] ++ [44])
+    ++ fmap (\x -> V2 x 20) ([11, 25, 26, 44])
+    ++ fmap (\x -> V2 x 21) ([12, 22 .. 24] ++ [44])
+    ++ fmap (\x -> V2 x 22) ([12, 20, 21, 44])
+    ++ fmap (\x -> V2 x 23) ([13, 17 .. 19] ++ [43])
+    ++ fmap (\x -> V2 x 24) ([13, 15, 16, 45])
 
 -- ....................................................
+zone3 =
+  fmap (\x -> V2 x 27) ([14, 39 .. 40] ++ [45])
+    ++ fmap (\x -> V2 x 28) ([14, 36 .. 38] ++ [45])
+    ++ fmap (\x -> V2 x 29) ([15, 33 .. 35] ++ [45])
+    ++ fmap (\x -> V2 x 30) ([15, 31, 32] ++ [45])
+    ++ fmap (\x -> V2 x 31) ([16, 28 .. 30] ++ [46])
+    ++ fmap (\x -> V2 x 32) ([16, 25 .. 27] ++ [46])
+    ++ fmap (\x -> V2 x 33) ([17, 23, 24] ++ [46])
+    ++ fmap (\x -> V2 x 34) ([17, 20 .. 22] ++ [46])
+    ++ fmap (\x -> V2 x 35) ([15, 33 .. 35] ++ [45])
+
+zone4 = []
+
+zone5 = []
+
+zone6 = []
+
 bgAttr, blackAttr, whiteAttr, boardAttr :: AttrName
 bgAttr = "bgAttr"
 blackAttr = "blackAttr"
